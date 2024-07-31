f265da087f28f0ec0e974a6ee486458c4db4b78e
Ju0011 <yhs07240@naver.com>
2024-07-31 18:54:15 +0900

    네이버지도_Blog_init

--- Blog/Blog_Croll.py
+++ Blog/Blog_Croll.py
@@ -5,7 +5,6 @@ import urllib.request
 import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import ssl
 
